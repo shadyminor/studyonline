53d621d82224815d1206530bff7fb22d5e04bd63
shadyminor <simon@hardy.ca>
2016-04-01 18:17:11 -0400

    Added files via upload

---
+++ js/behaviours.js
@@ -0,0 +1,442 @@
+﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2-vsdoc.js" />
+
+if (!oolp) {
+	var oolp = {};
+}
+
+oolp.api = {
+	key: '3a57cea5-7557-4909-a5d4-9f74a10c945b',
+	apiUrl: 'http://api.contactnorth.ca',
+	doMock: false,
+
+	getFilterOptions: function (lang, callback, errorCallback) {
+		if (oolp.api.doMock) {
+			var result = oolp.mock.getFilterOptions();
+			callback(result);
+		} else {
+			var data = { lang: lang };
+			oolp.api.call('getFilterOptions', data, callback, errorCallback);
+		}
+	},
+
+	performSearch: function (criteria, pageNumber, pageSize, callback, errorCallback) {
+		if (oolp.api.doMock) {
+			var result = oolp.mock.performSearch();
+			callback(result);
+		} else {
+			//	Assemble the params
+			var data = {
+				criteria: criteria,
+				paging: {
+					size: pageSize,
+					current: pageNumber
+				}
+			};
+
+			//	Make the call.
+			oolp.api.call('doSearch', data, callback, errorCallback);
+		}
+	},
+
+	getPrograms: function (ids, callback, errorCallback) {
+		if (oolp.api.doMock) {
+			var result = oolp.mock.getProgram();
+			callback(result);
+		} else {
+			//	Ensure the list of IDs is an array.
+			if (!$.isArray(ids)) {
+				ids = [ids];
+			}
+
+			//	Make the call.
+			oolp.api.call('getProgram', { id: ids }, callback, errorCallback);
+		}
+	},
+
+	getCourses: function (ids, callback, errorCallback) {
+		if (oolp.api.doMock) {
+			var result = oolp.mock.getCourse();
+			callback(result);
+		} else {
+			//	Ensure the list of IDs is an array.
+			if (!$.isArray(ids)) {
+				ids = [ids];
+			}
+
+			//	Make the call.
+			oolp.api.call('getCourse', { id: ids }, callback, errorCallback);
+		}
+	},
+
+	call: function (methodName, data, callback, errorCallback) {
+
+		//	Prepend Universal stuff
+		data = oolp.api.addUniversal(data);
+
+		//	Build the URL.
+		var url = oolp.api.buildUrl(methodName);
+
+		//	Make the call.
+		$.ajax({
+			url: url,
+			success: callback,
+			error: errorCallback,
+			data: JSON.stringify(data),
+			dataType: 'json',
+			type: 'POST',
+			crossDomain:true
+		});
+	},
+
+	addUniversal: function (data) {
+		if (!data.key) {
+			data.key = oolp.api.key;
+		}
+
+		if (!data.lang) {
+			data.lang = 'en'; // TODO: Set this to the actual lang.
+		}
+
+		return data;
+	},
+
+	buildUrl: function (methodName) {
+		return oolp.api.apiUrl + "/" + methodName;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+oolp.mock = {
+	getFilterOptions: function () {
+		return {
+			institution: [
+	{
+		id: 159,
+		name: "Algoma District School Board",
+		level: "Secondary",
+		url: "http://www.adsb.on.ca/content/secondary/index.asp?cat=52, www.adsb.on.ca"
+	},
+	{
+		id: 54,
+		name: "Algoma University",
+		level: "University",
+		url: "http://www.algomau.ca/view.php?page=ap_ptStudies"
+	},
+	{
+		id: 71,
+		name: "Algonquin and Lakeshore Catholic District School Board",
+		level: "Secondary",
+		url: "http://www.alcdsb.on.ca/"
+	},
+	{
+		id: 1,
+		name: "Algonquin College ",
+		level: "College",
+		url: "http://www.algonquincollege.com/PartTimeStudies/distanceEducation.htm"
+	},
+	{
+		id: 152,
+		name: "AlphaPlus Centre",
+		level: "Other",
+		url: "http://alphaplus.ca/"
+	}
+	],
+			semester: [
+	{
+		id: "Spring 2011",
+		label: "Spring 2011"
+	},
+	{
+		id: "Summer 2011",
+		label: "Summer 2011"
+	},
+	{
+		id: "Fall 2011",
+		label: "Fall 2011"
+	},
+	{
+		id: "Winter 2012",
+		label: "Winter 2012"
+	},
+	{
+		id: "Spring 2012",
+		label: "Spring 2012"
+	},
+	{
+		id: "Summer 2012",
+		label: "Summer 2012"
+	}
+	],
+			level: [
+	{
+		id: "College",
+		label: "College"
+	},
+	{
+		id: "University",
+		label: "University"
+	},
+	{
+		id: "Secondary",
+		label: "Secondary"
+	},
+	{
+		id: "Literacy",
+		label: "Literacy"
+	},
+	{
+		id: "Other",
+		label: "Training"
+	}
+	],
+			delivery_method: [
+	{
+		id: "online_live",
+		label: "Online Live"
+	},
+	{
+		id: "online_anytime",
+		label: "Online Anytime"
+	},
+	{
+		id: "correspondence",
+		label: "Correspondence"
+	},
+	{
+		id: "on_campus",
+		label: "On Campus"
+	},
+	{
+		id: "blended",
+		label: "Blended"
+	}
+	],
+			program_type: [
+	{
+		id: "certificate",
+		label: "Certificate"
+	},
+	{
+		id: "graduate_certificate",
+		label: "Graduate Certificate"
+	},
+	{
+		id: "diploma",
+		label: "Diploma"
+	},
+	{
+		id: "bachelors_degree",
+		label: "Bachelors Degree"
+	},
+	{
+		id: "recognition_of_achievement",
+		label: "Recognition of Achievement"
+	},
+	{
+		id: "advanced_diploma",
+		label: "Advanced Diploma"
+	},
+	{
+		id: "masters_degree",
+		label: "Masters Degree"
+	},
+	{
+		id: "doctoral_degree",
+		label: "Doctoral Degree"
+	}
+	],
+			field_of_study: [
+	{
+		id: "1",
+		label: "Academic Upgrading"
+	},
+	{
+		id: "2",
+		label: "Business"
+	},
+	{
+		id: "3",
+		label: "Communications"
+	},
+	{
+		id: "4",
+		label: "Computers / Networks"
+	},
+	{
+		id: "5",
+		label: "Health"
+	},
+	{
+		id: "6",
+		label: "High School"
+	},
+	{
+		id: "7",
+		label: "Literacy / Basic Skills"
+	},
+	{
+		id: "8",
+		label: "Personal Improvement"
+	},
+	{
+		id: "9",
+		label: "Professions"
+	},
+	{
+		id: "10",
+		label: "Science"
+	},
+	{
+		id: "11",
+		label: "Social Sciences"
+	},
+	{
+		id: "12",
+		label: "Trades / Technology"
+	}
+	]
+		};
+	},
+
+
+	getCourse: function () {
+		return [
+			{
+				id: 57440,
+				name: " Air Reservations",
+				code: "TRAV611",
+				description: "Learn the airfare terms, codes, ...etc",
+				credits: "2.00",
+				language: "en",
+				institution: 1,
+				prerequistes: "TRAV610",
+				delivery_method: ["online", "online_live"],
+				related_programs: [
+					{
+						id: 5525,
+						name: "Airline Products and Distribution"
+					},
+					{
+						id: 5529,
+						name: "Travel Agency Operations"
+					}
+				]
+			},
+			{
+				id: 50834,
+				name: "Abnormal Psychology",
+				code: "COUN2580",
+				description: "This course focuses on various...etc",
+				credits: "1.00",
+				language: "en",
+				institution: 6,
+				prerequistes: "PSYC 1051: PSYCHOLOGY INTRODUCTION",
+				delivery_method: ["online", "online_live"],
+				related_programs: [
+					{
+						id: 5054,
+						name: "Business - Entrepreneurship and Small Business"
+					}
+				]
+			}
+		];
+	},
+
+
+	getProgram: function () {
+		var result = [
+	{
+		id: 5037,
+		name: "911 and Emergency Response",
+		type: "certificate",
+		description: "Employment opportunities in this field are...etc",
+		full_time: 1,
+		delivery_method: [
+	"online",
+	"online_live"
+	],
+		institution: 152,
+		related_courses: [
+	{
+		id: 51444,
+		name: "911 and Police Dispatch"
+	},
+	{
+		id: 51445,
+		name: "911 Field Observation"
+	},
+	{
+		id: 51237,
+		name: "APCO Public Safety Telecommunicator"
+	}
+	]
+	},
+	{
+		id: 5160,
+		name: "Accounting",
+		type: "diploma",
+		description: "This diploma program is identical to the one...etc",
+		full_time: 1,
+		delivery_method: [
+	"correspondance",
+	"oncampus"
+	],
+		institution: 2,
+		related_courses: [
+	{
+		id: 53407,
+		name: "Corporate Finance I"
+	},
+	{
+		id: 53433,
+		name: "Financial Accounting I"
+	},
+	{
+		id: 53686,
+		name: "Financial Accounting II"
+	}
+	]
+	}
+	];
+		return result;
+	},
+	
+
+	performSearch: function () {
+		var result = {
+			programs: {
+				all_ids: [4836, 4290, 4840, 5060],
+				page_data: [
+								{
+									id: 4836,
+									name: "Business - Accounting",
+									type: "diploma",
+									level: "college",
+									institution: 1
+								}
+							]
+			},
+			courses: {
+				all_ids: [51541, 51545, 51546],
+				page_data: [
+						{
+							id: 51541,
+							name: "Accounting for Property Managers",
+							semester: "2011-2",
+							level: "college",
+							institution: 156
+						}
+					]
+			}
+		};
+		return result;
+	}
+}
---
+++ js/fakeData.js
@@ -0,0 +1,68 @@
+﻿var fakeData =
+[
+	{
+		programName: "Something",
+		programId: 1,
+		institution: "Algonquin College",
+		type: "Diploma",
+		descrip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a quam in leo auctor ornare id eget dui. Fusce condimentum consequat auctor. Nam nec ligula sed dui viverra porttitor. Praesent sit amet eros at magna facilisis pretium. Aenean vitae sollicitudin tortor. Proin vel arcu eros. Pellentesque massa libero, interdum sit amet mattis a, dictum eget neque. Fusce scelerisque iaculis arcu a consectetur. Fusce tellus dolor, rutrum a tincidunt vel, fermentum vel ipsum. Donec nibh lorem, facilisis at commodo et, volutpat nec lorem. Vivamus feugiat tellus eget erat varius ullamcorper. Donec quis neque sem. Nulla suscipit hendrerit eleifend. Suspendisse nec ipsum metus. Curabitur pretium ornare risus nec gravida. Aliquam blandit est in est condimentum dictum. Donec pulvinar adipiscing ante, non lacinia urna molestie et. Nulla facilisi. Mauris eu mi augue. Vestibulum laoreet nunc a est rutrum nec hendrerit turpis imperdiet.",
+		courses:
+		[
+			"foo",
+			"bar",
+			"welding"
+		]
+	}
+	,{
+		programName: "Something Else",
+		programId: 2,
+		institution: "Algonquin College",
+		type: "Diploma",
+		descrip: "Aenean eros arcu, sollicitudin a pretium sed, bibendum ac massa. Aliquam mauris nibh, cursus egestas lacinia quis, hendrerit eu nisi. Nulla convallis, metus et rhoncus dictum, nunc elit venenatis justo, vel consectetur velit justo non ante. Suspendisse potenti. Vivamus massa quam, placerat non dignissim in, sollicitudin sed lacus. Donec mattis dictum ipsum ac interdum. Praesent eu nulla arcu. Morbi porta pulvinar risus, non aliquam est semper eget. Vivamus metus magna, scelerisque ut varius eget, gravida vitae urna. Suspendisse imperdiet, felis at placerat dignissim, ligula ipsum vulputate turpis, sed sollicitudin risus turpis at justo. Cras eros ipsum, sollicitudin in tristique ac, venenatis quis diam. Vestibulum pretium ipsum at ante consequat sed dapibus velit eleifend. Suspendisse quis vehicula nunc. Praesent a mattis risus. Duis malesuada magna vestibulum mauris varius viverra.",
+		courses:
+		[
+			"foo",
+			"bar",
+			"welding"
+		]
+	}
+	,{
+		programName: "Too Tired to Think of Something Good",
+		programId: 3,
+		institution: "Algonquin College",
+		type: "Diploma",
+		descrip: "Aenean non tellus nisl. Quisque nec mauris nec est pretium pulvinar id sit amet turpis. Vestibulum pellentesque, dui et accumsan commodo, magna magna pretium urna, sed sodales sapien nisl sed nisi. Nulla auctor viverra suscipit. Nulla in sapien vitae neque dictum suscipit. Nulla ac turpis nisi. Nunc condimentum, ante at elementum sodales, dui elit molestie purus, et iaculis mauris ligula vel nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla in risus arcu, vitae auctor nibh. Duis lectus ligula, suscipit pretium feugiat a, pharetra at urna. Etiam elit eros, tempus sit amet blandit a, commodo at metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris sed nulla eget tellus facilisis egestas at eu tellus. Cras nibh felis, varius sed euismod at, feugiat ac est. Sed vestibulum, neque sit amet fringilla fringilla, massa dolor gravida magna, nec pretium nulla sem nec tortor. Integer ipsum massa, blandit nec posuere eget, accumsan ullamcorper felis. Vivamus id augue lorem, quis iaculis nunc.",
+		courses:
+		[
+			"foo",
+			"bar",
+			"welding"
+		]
+	}
+	,{
+		programName: "Oh Hai Jerry",
+		programId: 4,
+		institution: "Algonquin College",
+		type: "Diploma",
+		descrip: "Maecenas viverra turpis sed justo imperdiet consectetur luctus nisi venenatis. Vivamus adipiscing porta faucibus. Duis eu nunc nunc, quis elementum ligula. Integer ullamcorper blandit elit, sed dapibus odio molestie ut. Sed aliquet nisi ut metus volutpat ultrices. Phasellus molestie urna vitae justo ultrices laoreet. Phasellus eget eros mi, id imperdiet ante. Curabitur porttitor laoreet purus eget tristique. Vestibulum sollicitudin enim eget massa facilisis eleifend. Phasellus ut ullamcorper felis. Sed cursus accumsan dictum. Vivamus sed turpis sem. Vivamus congue enim at augue scelerisque nec aliquam tellus venenatis. Donec pellentesque sem vitae erat euismod et semper elit tristique. Donec consequat fringilla nunc ac ultricies. Donec et ligula ut urna lobortis bibendum vel sed quam. Suspendisse vel felis non urna egestas lacinia. Duis sagittis turpis vel velit commodo ultrices. Fusce ullamcorper gravida ornare.",
+		courses:
+		[
+			"foo",
+			"bar",
+			"welding"
+		]
+	}
+	,{
+		programName: "Google",
+		programId: 5,
+		institution: "Algonquin College",
+		type: "Diploma",
+		descrip: "Nam vehicula viverra dolor, et aliquet urna sagittis quis. Vivamus sit amet erat tellus. Nam mattis ante et nunc egestas vitae dapibus elit varius. Nam auctor tristique neque, quis suscipit velit condimentum id. Sed elit magna, rutrum eu fermentum quis, tempor a erat. Vestibulum massa risus, fermentum id scelerisque vitae, porta in massa. Ut adipiscing, velit tristique faucibus adipiscing, tortor tortor adipiscing felis, ut auctor lectus diam ullamcorper nisi. Fusce id mollis leo. Proin pretium odio a arcu sodales in iaculis neque ornare. Morbi mattis velit nec tortor pulvinar vitae fermentum nisl accumsan. Vivamus elit justo, molestie vulputate imperdiet auctor, dictum ut nulla. Nulla malesuada nunc ac elit auctor lacinia. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur lorem lacus, elementum at tincidunt id, lobortis quis libero. Ut non quam ullamcorper mi egestas consequat a sed dui. Duis arcu sapien, fermentum vitae fringilla in, imperdiet sit amet dolor.",
+		courses:
+		[
+			"foo",
+			"bar",
+			"welding"
+		]
+	}
+]
